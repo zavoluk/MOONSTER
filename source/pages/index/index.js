99cb6852cbe7c6a03fd17d9012b4ca74e574bdef
zavoluk <zavoluk@gmail.com>
2018-11-09 23:42:51 +0300

    setting environment

---
+++ source/pages/index/index.js
@@ -0,0 +1,12 @@
+/**
+ * style
+ */
+import '../../styles/reset.sass'
+import '../../styles/fonts.sass'
+import '../../styles/global.sass'
+
+import '../../components/logo/logo.sass'
+
+/**
+ * js
+ */
---
+++ webpack/file-loader.js
@@ -0,0 +1,41 @@
+module.exports = () => {
+	return {
+		module: {
+			rules: [
+				{
+					test: /\.(png|jpg|gif|svg)$/,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: 'img/[name].[ext]'
+							}
+						}
+					]
+				},
+				{
+					test: /\.(ico)$/,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: '[name].[ext]'
+							}
+						}
+					]
+				},
+				{
+					test: /\.(ttf)$/,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: 'fonts/[name].[ext]'
+							}
+						}
+					]
+				}
+			]
+		}
+	}
+}
