eb21f8690232556176a09e152ce2013231ca3689
zavoluk <zavoluk@gmail.com>
2018-11-13 00:30:23 +0300

    slide size, slide as nav, viewport scale

--- source/components/carousel/carousel.js
+++ source/components/carousel/carousel.js
@@ -1,7 +1,23 @@
 import Swiper from 'swiper'
 
-new Swiper('.swiper-container', {
+const mySwiper = new Swiper('.swiper-container', {
 	slidesPerView: 'auto',
 	centeredSlides: true,
-	loop: true,
+	loop: true
 })
+
+let slide = document.querySelectorAll('.swiper-slide')
+
+for (let i = 0; i < slide.length; i++) {
+	slide[i].addEventListener('click', (e) => {
+		let current = e.target
+
+		if (current.classList.contains('swiper-slide-prev')) {
+			mySwiper.slidePrev()
+		}
+
+		if (current.classList.contains('swiper-slide-next')) {
+			mySwiper.slideNext()
+		}
+	})
+}
